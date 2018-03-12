7524df2be6d5cfca8d1905981efa33bd6619addf
davidc <dcastannon@safecreative.org>
2018-03-12 17:46:09 +0100

    wip

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# go-uid32
+# go-uids
+
+
+
+NOT FOR PRODUCTION USE
+
+
+uuid16r (reverse)
+
+128bits universal unique id lexicographically ordered, url safe, time based with auto clock correction 
+and strictly decremental
+
+Sample
+
+    ~~~cM6ajvtg~0smDUwh41l
+    ~~~cM6ajuE7~pUB6IO65MW
+    ~~~cM6ajsXR~r6WT3ORHvG
+    ~~~c4VxsK_o~bUjT6AH7X0
+
+i.e.:
+
+    cM6ajvtg~0smDUwh41l
+    cM6ajuE7~pUB6IO65MW
+    cM6ajsXR~r6WT3ORHvG
+    c4VxsK_o~bUjT6AH7X0
+
+
---
+++ uid16r_test.go
@@ -0,0 +1,126 @@
+package uids
+
+import (
+	"testing"
+	"fmt"
+	"time"
+	"math/rand"
+)
+
+func TestGenerate(t *testing.T) {
+	g := NewUId16rGen()
+	v := g.New()
+	v2, _ := g.FromString(v.String())
+	chkEqual(t, v.String(), v2.String())
+}
+
+func TestVariableEncoding(t *testing.T) {
+	g := NewUId16rGen()
+	v1, err := g.FromString("~~~k~Hb3IAk2WZ1XkT3feG")
+	chk(t, err)
+	v2, err := g.FromString("k~Hb3IAk2WZ1XkT3feG")
+	chk(t, err)
+	chkEqual(t, v1.String(), v2.String())
+}
+
+func BenchmarkGenerate(b *testing.B) {
+	g := NewUId16rGen()
+	for i := 0; i < 1000000; i++ {
+		g.New()
+	}
+}
+
+func TestExampleGenerate(t *testing.T) {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	g := NewUId16rGen()
+	for i := 0; i < 10; i++ {
+		id := g.New()
+		fmt.Println(id.String())
+	}
+	//// Output: hello
+}
+
+func TestShorten(t *testing.T) {
+	g := NewUId16rGen()
+	v1, err := g.FromString("~~~k~Hb3IAk2WZ1XkT3feG")
+	chk(t, err)
+	v2 := v1.Shorten()
+	chkEqual(t, "k~Hb3IAk2WZ1XkT3feG", v2)
+}
+
+func TestMax(t *testing.T) {
+	g := NewUId16rGen()
+	v1 := maxId16r()
+	chkEqual(t, "~~~~~~~~~~~~~~~~~~~~~l", v1.String())
+	v2 := v1.Shorten()
+	chkEqual(t, "l", v2)
+	v3, err := g.FromString(v1.String())
+	chk(t, err)
+	chkEqual(t, v1.String(), v3.String())
+}
+
+func TestMin(t *testing.T) {
+	v1 := minId16r()
+	chkEqual(t, "0000000000000000000000", v1.String())
+	g := NewUId16rGen()
+	v2, err := g.FromString(v1.String())
+	chk(t, err)
+	chkEqual(t, v1.String(), v2.String())
+
+}
+
+func TestVoid(t *testing.T) {
+	g := NewUId16rGen()
+	v1, err := g.FromString("")
+	chk(t, err)
+	s1 := v1.String()
+	chkEqual(t, "~~~~~~~~~~~~~~~~~~~~~l", s1)
+	v2 := v1.Shorten()
+	chkEqual(t, "l", v2)
+}
+func TestVoid2(t *testing.T) {
+	g := NewUId16rGen()
+	v1, err := g.FromString("~")
+	chk(t, err)
+	s1 := v1.String()
+	chkEqual(t, "~~~~~~~~~~~~~~~~~~~~~l", s1)
+	v2 := v1.Shorten()
+	chkEqual(t, "l", v2)
+}
+
+func TestIncSeq(t *testing.T) {
+	timeFunc := func() uint64 {
+		return 1
+	}
+	randFunc := func(b []byte) {
+		for i := 0; i < len(b); i++ {
+			b[i] = 0
+		}
+	}
+	g := UId16rGen{
+		lastSeq:  0xff,
+		timeFunc: timeFunc,
+		randFunc: randFunc,
+	}
+
+	v1 := g.New()
+	chkEqual(t, "~~~~~~~~~~w~0000000000", v1.String())
+	v2 := g.New()
+	chkEqual(t, "~~~~~~~~~~wz0000000000", v2.String())
+
+}
+
+func chk(t *testing.T, err error) {
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+}
+
+func chkEqual(t *testing.T, v1 string, v2 string) {
+	if v1 != v2 {
+		t.Error(fmt.Sprintf("expected [%s] but was [%s]", v1, v2))
+		t.FailNow()
+	}
+}
